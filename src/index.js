fd139244dc5b4db6034efb2648a3c46376f9c13b
Пользователь <artemkinartem6@gmail.com>
2022-07-13 08:14:13 +0300

    init: add new file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+module.exports = function towelSort (matrix) {
+    if (!matrix) return [];
+  
+    let result = [];
+    matrix.forEach((element, index) => {
+    if (index % 2 === 0) {
+        result.push(element.sort((a, b) => {
+          return a - b;
+        }));
+      } else {
+        result.push(element.sort((a, b) => {
+          return b - a;
+        }));
+      }
+    });
+    result = [].concat(...result);
+    return result;
+  }
